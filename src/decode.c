484d659c0826d806ddf74f2403e399a1df626103
Gabriel Ford <gabe@fordltc.net>
2025-04-14 12:19:22 -0400

    Convert png int to little endian.

--- src/decode.c
+++ src/decode.c
@@ -1,4 +1,5 @@
 #include "decode.h"
+#include <stdint.h>
 #include <stdio.h>
 
 static bool at_ihdr_label(uint8_t *data, int i) {
@@ -13,12 +14,28 @@ static bool at_ihdr_label(uint8_t *data, int i) {
   return true;
 }
 
+// Reads Big-Endian png int to Little-Endian.
+uint32_t get_png_int(uint8_t *data) {
+  uint32_t png_int = 0;
+
+  png_int |= (data[0] << (3 * 8));
+  png_int |= (data[1] << (2 * 8));
+  png_int |= (data[2] << (1 * 8));
+  png_int |= (data[3] << (0 * 8));
+
+  return png_int;
+}
+
+#define WIDTH_OFFSET 4
+#define HEIGHT_OFFSET 8
+
 bool read_image_header(uint8_t *data, struct image_header *image_header) {
   int i = 0;
   while (!at_ihdr_label(data, i))
     i++;
 
-  printf("Found Image Header at byte %i", i);
+  image_header->width = get_png_int(&data[i + WIDTH_OFFSET]);
+  image_header->height = get_png_int(&data[i + HEIGHT_OFFSET]);
 
   return true;
 }
